21c496016a7a4d7cf458ff7b3c8fe77c573c795b
Mootassam <mootassame@gmail.com>
2023-01-25 23:12:49 +0100

    edit the source code

--- src/components/navigation/NavigationLogo.js
+++ src/components/navigation/NavigationLogo.js
@@ -1,18 +1,17 @@
 import { NavLink } from "react-router-dom";
 import IconBlack from "../../assets/images/icon-black.png";
-import ResetLocation from '../../helpers/ResetLocation'
+import ResetLocation from "../../helpers/ResetLocation";
 
 const NavigationLogo = (props) => {
   return (
     <NavLink
-      to="/"
-      className="logo-section"
+      to='/'
+      className='logo-section'
       onClick={() => {
         ResetLocation();
         props.closeMenu();
-      }}
-    >
-      <img src={IconBlack} alt="logo"></img>
+      }}>
+      <img src={IconBlack} alt='logo'></img>
     </NavLink>
   );
 };
